fef51a07631b8038badf2fde55b1355f5d33d719
Patrick van de Glind <patrick.glind@hds.com>
2015-06-01 12:03:38 +0200

    Updated Item struct to allow Array of ItemEnclosure
    And added a test RSS file to ensure it is working

--- rss.go
+++ rss.go
@@ -35,15 +35,15 @@ type ItemEnclosure struct {
 }
 
 type Item struct {
-	Title       string        `xml:"title"`
-	Link        string        `xml:"link"`
-	Comments    string        `xml:"comments"`
-	PubDate     Date          `xml:"pubDate"`
-	GUID        string        `xml:"guid"`
-	Category    []string      `xml:"category"`
-	Enclosure   ItemEnclosure `xml:"enclosure"`
-	Description string        `xml:"description"`
-	Content     string        `xml:"content"`
+	Title       string          `xml:"title"`
+	Link        string          `xml:"link"`
+	Comments    string          `xml:"comments"`
+	PubDate     Date            `xml:"pubDate"`
+	GUID        string          `xml:"guid"`
+	Category    []string        `xml:"category"`
+	Enclosure   []ItemEnclosure `xml:"enclosure"`
+	Description string          `xml:"description"`
+	Content     string          `xml:"content"`
 }
 
 type Date string
