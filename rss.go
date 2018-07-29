5c7a4e26da123f9be98cfd791a2573171d435485
Ruslan Zalalov <zalalov@mail.ru>
2018-07-30 00:57:24 +0300

    Author field added.

--- rss.go
+++ rss.go
@@ -46,6 +46,7 @@ type Item struct {
 	Category    []string        `xml:"category"`
 	Enclosure   []ItemEnclosure `xml:"enclosure"`
 	Description string          `xml:"description"`
+	Author 		string          `xml:"author"`
 	Content     string          `xml:"content"`
 	FullText    string          `xml:"full-text"`
 }
